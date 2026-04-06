212393fecf5a58591423426222929585e2842fa5
Nilton Mana <Niltonmana11@gmail.com>
2026-04-06 13:18:36 +0200

    ajout testInsert dans TestDaoTypePiece (test PKException et CheckException)

--- tests/org/isfce/pdb/dao/TestDaoTypePiece.java
+++ tests/org/isfce/pdb/dao/TestDaoTypePiece.java
@@ -1,6 +1,7 @@
 package org.isfce.pdb.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.SQLException;
@@ -9,6 +10,8 @@ import org.isfce.pdb.dao.DAOFactory.TypePersistance;
 import org.isfce.pdb.databases.connexion.ConnexionFromFile;
 import org.isfce.pdb.databases.connexion.ConnexionSingleton;
 import org.isfce.pdb.databases.uri.Databases;
+import org.isfce.pdb.exceptions.CheckException;
+import org.isfce.pdb.exceptions.PKException;
 import org.isfce.pdb.model.TypePiece;
 import org.isfce.pdb.util.DatabaseUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -49,6 +52,17 @@ public class TestDaoTypePiece {
 		var liste = dao.getListe(null);
 		assertTrue(liste.size() == 3);
 		assertEquals(p, liste.get(1));
+	}
+	
+	@Test 
+	void testInsert() {
+		// Test 1 : doublon de clé primaire -> doit lever PKException
+		TypePiece tp1 = new TypePiece("SALON", "doublon!", false);
+		assertThrows(PKException.class, () -> dao.insert(tp1));
+		
+		// Test 2 : nom vide -> viole contrainte CHECK -> doit lever CheckException
+		TypePiece tp2 = new TypePiece("BROL", "", false);
+		assertThrows(CheckException.class, () -> dao.insert(tp2));
 	}
 
 }
