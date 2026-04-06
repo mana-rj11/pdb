e87cb67b7ab7d7fa64f598fc06a3e5e310804efa
Nilton Mana <Niltonmana11@gmail.com>
2026-04-06 12:05:16 +0200

    mis à jour dispatchException (gestion PKException et CheckException Firebird

--- src/org/isfce/pdb/dao/FBDAOFactory.java
+++ src/org/isfce/pdb/dao/FBDAOFactory.java
@@ -1,8 +1,11 @@
 package org.isfce.pdb.dao;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
+import org.isfce.pdb.exceptions.CheckException;
 import org.isfce.pdb.exceptions.InstallationException;
+import org.isfce.pdb.exceptions.PKException;
 
 public class FBDAOFactory extends DAOFactory {
 	private Connection connexion;
@@ -24,9 +27,19 @@ public class FBDAOFactory extends DAOFactory {
 		return connexion;
 	}
 
+	// recuperer le code d'erreur firebird avec exc.getError...
+	// PKException
+	// CheckException, violée
+	// default = autres erreur SQL
 	@Override
 	protected void dispatchException(Exception e, String detail) throws InstallationException {
-		throw new InstallationException(" Problème ");
+		SQLException exc = (SQLException) e;
+		
+		throw switch (exc.getErrorCode()) {
+		case 335544665 -> new PKException(e.getMessage(), detail);
+		case 335544347 -> new CheckException(e.getMessage(), detail);
+		default -> new InstallationException("Problème");
+		};
 	}
 	
 	private IPieceDao daoPiece = null;
